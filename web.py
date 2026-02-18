f6426e1f08df03329b38f039c108050949c1fcb7
Jihane <jihanerahme@hotmail.com>
2026-02-18 23:16:35 +0200

    with edit

--- web.py
+++ web.py
@@ -12,14 +12,34 @@ st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
-
 for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=todo)
+    col1, col2, col3 = st.columns([6, 1, 1])
+
+    # Checkbox column
+    with col1:
+        checkbox = st.checkbox(todo.strip(), key=f"check_{index}")
+
+    # Edit button column
+    with col2:
+        if st.button("✏️", key=f"edit_{index}"):
+            st.session_state[f"editing_{index}"] = True
+
+    # Delete if checked
     if checkbox:
         todos.pop(index)
         functions.write_todos(todos)
-        del st.session_state[todo]
         st.rerun()
 
-st.text_input(label="", placeholder="Add new todo...",
-              on_change=add_todo, key='new_todo')
+    # Show edit input if editing
+    if st.session_state.get(f"editing_{index}", False):
+        new_value = st.text_input(
+            "Edit todo:",
+            value=todo.strip(),
+            key=f"input_{index}"
+        )
+
+        if st.button("Save", key=f"save_{index}"):
+            todos[index] = new_value + "\n"
+            functions.write_todos(todos)
+            st.session_state[f"editing_{index}"] = False
+            st.rerun()
